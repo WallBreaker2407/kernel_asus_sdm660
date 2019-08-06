7e315ea2098743078a954bb88cdedf0dd91b3c7a
Julian Liu <wlootlxt123@gmail.com>
2019-08-06 16:36:11 +0800

    qpnp-fg-gen3: force setting warm/hot jeita threshold as 97C

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -5164,6 +5164,10 @@ static int fg_parse_dt(struct fg_chip *chip)
 			pr_warn("Error reading Jeita thresholds, default values will be used rc:%d\n",
 				rc);
 	}
+/* Huaqin modify for ZQL1650 Realize HW Jeita by fangaijun	at 2018/02/6 start */
+	printk("enter fg_parse_dt :HW jeita cold:%d,cool:%d,warm:%d,hot:%d\n", chip->dt.jeita_thresholds[JEITA_COLD],chip->dt.jeita_thresholds[JEITA_COOL] ,chip->dt.jeita_thresholds[JEITA_WARM],chip->dt.jeita_thresholds[JEITA_HOT]); 
+/* Huaqin modify for ZQL1650 Realize HW Jeita by fangaijun	at 2018/02/6 end */
+
 	chip->dt.jeita_thresholds[JEITA_WARM] = 97;
 	chip->dt.jeita_thresholds[JEITA_HOT] = 97;
 
